3be14b6191085510bb322693bc436985b900130f
MadAlex1997 <kf5kfj@gmail.com>
2023-09-03 13:32:08 -0400

    Setup so that it only loads 10 minutes of data at at a time. Improved from 4 fps to 30+

--- plot.py
+++ plot.py
@@ -5,7 +5,7 @@ from scipy.io import wavfile
 import numpy as np
 import warnings
 import plotly.graph_objects as go
-
+import datetime
 warnings.filterwarnings("ignore")
 
 dpg.create_context()
@@ -22,13 +22,24 @@ def callback(sender, app_data):
 def get_data(sender, app_data):
     print(app_data)
     file_path_string = dpg.get_value("file_path")
-
+    sl1 = 0
+    sl2 = 600*1000
+    sl = 0
+    if sender == "tslice":
+        delete_waveform()
+        sl = app_data
+        sl1 = app_data*600*1000
+        sl2 = (app_data+1)*600*1000
     if ".wav" in file_path_string:
         fs, data = wavfile.read(file_path_string)
-        if data.shape[1]>1:
-            data = data.mean(axis=1)
         length = data.shape[0] / fs
         time = np.linspace(0., length, data.shape[0])
+        if data.shape[1]>1:
+            data = data.mean(axis=1)
+        
+        data = data[sl1:sl2]
+        time = time[sl1:sl2]
+        
         x_arr = list(time)
         y_arr = list(data)
         dpg.set_value("file_type","WAV")
@@ -39,9 +50,13 @@ def get_data(sender, app_data):
     else:
         try:
             collect = sig_read(file_path_string)[0]
-            x_arr = list(collect.times())
-            y_arr = list(collect.data)
-            dpg.set_value("file_type","WAV")
+            data = collect.data
+            time = collect.times()
+            data = data[sl1:sl2]
+            time = time[sl1:sl2]
+            x_arr = list(time)
+            y_arr = list(data)
+            dpg.set_value("file_type","File readable by obspy")
             dpg.set_value("x_array",x_arr)
             dpg.set_value("y_array",y_arr)
             dpg.set_value("fs",collect.stats.sampling_rate)
@@ -50,23 +65,26 @@ def get_data(sender, app_data):
             return 1
     
 
-    with dpg.window(label="Waveform",height=200,width=800,pos=[0,100],on_close=delete_waveform()):
+    with dpg.window(label="Waveform",tag="Waveform",height=200,width=800,pos=[0,150],on_close=delete_waveform()):
         def create_plot():
             with dpg.plot(label="Line Series", height=-1, width=-1,parent="Waveform"):
                 dpg.add_plot_legend(parent="Line Series")
                 # dpg.add_drag_line(label="dline1", color=[255, 0, 0, 255], default_value=2.0, callback=print_val)
                 dpg.add_plot_axis(dpg.mvXAxis, label="x",tag="xaxis_tag")
                 dpg.add_plot_axis(dpg.mvYAxis, label="y", tag="yaxis")
-                dpg.set_axis_limits("xaxis_tag", 0, 600)
+                dpg.set_axis_limits("xaxis_tag", sl*600, (sl+1)*600)
+                
                 # series 1
                 # dpg.add_line_series(x=x_arr, y=y_arr, label="series 1", parent="yaxis", tag="series_1")
                 dpg.add_line_series(x=dpg.get_value("x_array"), y=dpg.get_value("y_array"), label="series 1", parent="yaxis", tag="series_1")
+                # dpg.set_axis_ticks(axis="xaxis_tag",label_pairs=label_pairs)
                 # create plot
         
         create_plot()
         
 def bound(sender, app_data, user_data):
     dpg.set_axis_limits("xaxis_tag", app_data*600, (1+app_data)*600)
+    dpg.set
         
 def spectro(sender, app_data):
     data = np.array(dpg.get_value("y_array"))
@@ -100,18 +118,19 @@ with dpg.value_registry():
     dpg.add_float_vect_value(label="x_array",tag="x_array",default_value=[1.0 for i in range(100)])
     dpg.add_float_value(label="fs",tag="fs",default_value=0.0)
 
-with dpg.window(label="Tutorial", width=800, height=100):
+with dpg.window(label="Controls",tag="Controls", width=800, height=100):
     
     dpg.add_button(label="File Selector", callback=lambda: dpg.show_item("file_dialog_tag"))
     dpg.add_text(label="file_name",default_value="No file loaded",tag="file_name")
     dpg.add_text(label="file_type",default_value="No file loaded",tag="file_type")
     dpg.add_button(label="Plot Waveform", callback=get_data)
-    dpg.add_input_int(label="Time Slice",tag="tslice",default_value=0,callback=bound)
+    dpg.add_input_int(label="Time Slice",tag="tslice",default_value=0,callback=get_data)
     dpg.add_button(label="Spectrogram",callback=spectro)
 
 dpg.show_metrics()
 dpg.create_viewport(title='Custom Title',height=300,width=800)
 dpg.setup_dearpygui()
 dpg.show_viewport()
+dpg.set_primary_window("Controls",True)
 dpg.start_dearpygui()
 dpg.destroy_context()
